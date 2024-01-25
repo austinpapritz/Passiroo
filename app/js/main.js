0b00de25f2d6c72701a8b8531a32940cfe649461
Austin Papritz <austin@papritz.dev>
2024-01-24 23:53:34 -0800

    added login-user function to main.py

--- app/js/main.js
+++ app/js/main.js
@@ -33,3 +33,20 @@ ipcMain.on('register', (event, userData) => {
       event.reply('register-reply', data.toString());
     });
 });
+
+ipcMain.on('login', (event, userData) => {
+  const pythonScriptPath = path.join(__dirname, '../../py/main.py');
+    const pyProcess = spawn('python', [pythonScriptPath, 'login_user', userData.username, userData.password]);
+
+    pyProcess.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+  });
+  
+  pyProcess.on('close', (code) => {
+      console.log(`Python script exited with code ${code}`);
+  });
+    
+    pyProcess.stdout.on('data', (data) => {
+      event.reply('login-reply', data.toString());
+    });
+});
